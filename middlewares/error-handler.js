93b541494349b511e0c788dd6940f40ea0ebccf0
Syed Farhan <syedfarhan596@gmail.com>
2023-12-11 10:35:46 +0530

    commited by syed farhan

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,7 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 
 const ErrorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
   const customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong try again later",
